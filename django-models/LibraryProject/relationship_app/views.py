df1460da1bd362a16278aae6ffc46b784e84de06
Evans Omia <evansomia005@gmail.com>
2025-11-16 02:09:16 +0400

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -17,6 +17,6 @@ class LibraryDetailView(DetailView):
   context_object_name = "library"
   
   def get_context_data():
-    context= super().get_context_data(self, **kwargs)
+    context= super().get_context_data( **kwargs)
     context["books"] = self.object.books.all()
     return context
