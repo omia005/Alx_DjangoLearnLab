20e8f54d332d820fd8076aea74b8314758999c63
Evans Omia <evansomia005@gmail.com>
2025-11-16 01:55:15 +0400

    Create urls.py

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import LibraryDetailView, list_books
+
+urlpatterns = [
+    path("/library_details", LibraryDetailView.as_view(), name="library_detail"),
+    path("/list_books", list_books.as_view, name="list_books")
+]
