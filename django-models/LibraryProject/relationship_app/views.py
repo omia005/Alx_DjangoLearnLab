f3c3350f51357cf0ca18e77c475f337f0b7984eb
Evans Omia <evansomia005@gmail.com>
2025-11-16 01:37:06 +0400

    Create a class-based view in relationship_app/views.py that displays details for a specific library, listing all books available in that library.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from ./models.py import Library, Book, Author, Librarian
 
 # Create your views here.
+
+#view for listing all boks in the Library
 def list_books(request):
   books = Book.objects.all()
   context = {"Books":books}
   
-  
+  return render(request, "relationship_app/list_books.html", context)
 
-return render(request, "relationship_app/list_books.html", context)
+class LibraryDetailView(DetailView):
+  model = Library
+  template_name = relationship_app/library_detail.html
+
+  def get_context_data():
+    context= super().get_context_data(self, **kwargs)
+    context["books"] = self.object.books.all()
+    return context
