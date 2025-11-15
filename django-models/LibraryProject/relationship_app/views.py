b14c569d0c69013b1e9c43964718734ae1e65d08
Evans Omia <evansomia005@gmail.com>
2025-11-16 02:12:13 +0400

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -13,7 +13,7 @@ def list_books(request):
 
 class LibraryDetailView(DetailView):
   model = Library
-  template_name = relationship_app/library_detail.html
+  template_name = 'relationship_app/library_detail.html'
   context_object_name = "library"
   
   def get_context_data():
