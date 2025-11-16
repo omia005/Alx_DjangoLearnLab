1ffe799ed1d93a5de7933d896343394582a1253e
Evans Omia <evansomia005@gmail.com>
2025-11-16 10:50:51 +0400

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("library_details/", views.LibraryDetailView.as_view(), name="library_detail"),
     path("list_books/", views.list_books.as_view, name="list_books"),
     path("register/", views.register.as_view(), name="register"),
-    path("login/", LoginView.as_view(), template_name="login.html"),
-    path("logout/", LogoutView.as_view(), template_name="logout.html"),
+    path("login/", LoginView.as_view(template_name="login.html"), name="login" ),
+    path("logout/", LogoutView.as_view(template_name="logout.html"), name="logout" ),
 ]
