582f096ac951612527819976c01a9367f3e3a1d0
Evans Omia <evansomia005@gmail.com>
2025-11-16 23:41:54 +0400

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,9 +1,18 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django import forms
 from .models import Book
 from .forms import ExampleForm
 
-class ExampleForm(forms.ModelForm):
-    
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'description']
+@login_required
+def example_form_view(request):
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = ExampleForm()
+
+    return render(request, "bookshelf/form_example.html", {"form": form})
+
