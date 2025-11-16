1f414315044fafdf83b8474753a65102efa4532a
Evans Omia <evansomia005@gmail.com>
2025-11-16 09:55:23 +0400

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Library, Book, Author, Librarian
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 # Create your views here.
 
@@ -11,6 +14,7 @@ def list_books(request):
   
   return render(request, "relationship_app/list_books.html", context)
 
+#view for providing library details
 class LibraryDetailView(DetailView):
   model = Library
   template_name = 'relationship_app/library_detail.html'
@@ -20,3 +24,10 @@ class LibraryDetailView(DetailView):
     context= super().get_context_data( **kwargs)
     context["books"] = self.object.books.all()
     return context
+
+class RegistrationView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relashionship_app/register.html'
+
+
