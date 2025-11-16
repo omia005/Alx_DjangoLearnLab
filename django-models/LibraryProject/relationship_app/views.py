91d2fd8277eb065885bd7f3a5f47f8592777b7b8
Evans Omia <evansomia005@gmail.com>
2025-11-16 10:27:31 +0400

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
 from .models import Library, Book, Author, Librarian
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -26,8 +31,34 @@ class LibraryDetailView(DetailView):
     return context
 
 class RegistrationView(CreateView):
+    model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relashionship_app/register.html'
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password.")
+            return redirect("login")
+
+    return render(request, "relationship_app/login.html")
+
+@login_required
+class LogoutPageView(TemplateView):
+    template_name = "logout.html"
+
+    def get(self, request, *args, **kwargs):
+        logout(request)  # log the user out
+        return super().get(request, *args, **kwargs)
+
 
 
