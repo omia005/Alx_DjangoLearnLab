46050de5256f475cabf477c922d37de991d86479
Evans Omia <evansomia005@gmail.com>
2025-11-16 13:14:54 +0400

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('admin-dashboard/', admin_view.as_view(), name='admin_view'),
     path('librarian-dashboard/', librarian_view.as_view(), name='librarian_view'),
     path('member-dashboard/', member_view.as_view(), name='member_view'),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/', views.edit_book, name='edit_book'),
+    path('books/delete/', views.delete_book, name='delete_book'),
 ]
