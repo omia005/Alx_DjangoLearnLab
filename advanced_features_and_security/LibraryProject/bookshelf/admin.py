0fa0706409616045a44ba8607fb8cbb135e5ffbe
Evans Omia <evansomia005@gmail.com>
2025-11-16 15:21:59 +0400

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,7 +2,47 @@ from django.contrib import admin
 from .models import Book
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.urls import reverse_lazy
 
+
+
+# View Books (requires can_view)
+class BookListView(PermissionRequiredMixin, ListView):
+    model = Book
+    template_name = "books/book_list.html"
+    permission_required = "bookshelf.can_view"
+
+
+# Create Book (requires can_create)
+
+class BookCreateView(PermissionRequiredMixin, CreateView):
+    model = Book
+    fields = ["title", "author", "published_year"]
+    template_name = "books/book_form.html"
+    success_url = reverse_lazy("book_list")
+    permission_required = "bookshelf.can_create"
+
+
+# Edit Book (requires can_edit)
+
+class BookUpdateView(PermissionRequiredMixin, UpdateView):
+    model = Book
+    fields = ["title", "author", "published_year"]
+    template_name = "books/book_form.html"
+    success_url = reverse_lazy("book_list")
+    permission_required = "bookshelf.can_edit"
+
+
+
+# Delete Book (requires can_delete)
+
+class BookDeleteView(PermissionRequiredMixin, DeleteView):
+    model = Book
+    template_name = "books/book_confirm_delete.html"
+    success_url = reverse_lazy("book_list")
+    permission_required = "bookshelf.can_delete"
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
