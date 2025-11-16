922e83c964e8659b41c5da651611d1d4f9c64b00
Evans Omia <evansomia005@gmail.com>
2025-11-16 15:10:45 +0400

    Update admin.py

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    # Fields visible in admin user detail page
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional Info",
+            {
+                "fields": (
+                    "date_of_birth",
+                    "profile_photo",
+                )
+            },
+        ),
+    )
+
+    # Fields shown when creating new users in admin
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional Info",
+            {
+                "fields": (
+                    "date_of_birth",
+                    "profile_photo",
+                )
+            },
+        ),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
