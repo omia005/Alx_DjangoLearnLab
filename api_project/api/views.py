e92cc585af3e48c6d2b36996ff05cb28837bf321
Evans Omia <evansomia005@gmail.com>
2025-11-29 01:12:08 +0400

    changed the name of the view to BookList

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,6 +4,6 @@ from .models import Book
 from .serializers import BookSerializer
 
 # Create your views here.
-class BookListAPIView(generics.ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
