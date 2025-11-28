7e63177003b4c6f098dcf0ef12bd6538383a4623
Evans Omia <evansomia005@gmail.com>
2025-11-29 01:31:43 +0400

    created a viewset and registered it in urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookList
+from .views import BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 ]
