abbd8b641454e18675ae971aea00500e9f36b923
Evans Omia <evansomia005@gmail.com>
2025-12-11 11:59:37 +0400

    configured mysql database and corrected the use of in-built user model

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog_db',
+        'USER' : 'root',
+        'PASSWORD': '0938@2022Ab',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
@@ -116,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "django_blog" / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
