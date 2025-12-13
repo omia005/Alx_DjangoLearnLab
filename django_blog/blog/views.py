f1f80fcc3c3a80ad2cfdb4ad0be7a37b02e82926
Evans Omia <evansomia005@gmail.com>
2025-12-13 11:05:07 +0400

    created the templates for home , login, logout and registeration and configured their urls

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login_view(request):
+    return render(request, 'login.html')
+
+def logout_view(request):
+    return render(request, 'logout.html')
+
+def register_view(request):
+    return render(request, 'register.html')
+
+def profile_view(request):
+    return render(request, 'profile.html')
+
+def home_view(request):
+    return render(request, 'home.html')
