a4fc494ff0f5884c09bd11abea7a4600eea7b072
Evans Omia <evansomia005@gmail.com>
2025-12-13 20:50:45 +0400

    created the user registration form

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def login_view(request):
-    return render(request, 'login.html')
+    return render(request, 'blog/login.html')
 
 def logout_view(request):
-    return render(request, 'logout.html')
+    return render(request, 'blog/logout.html')
 
 def register_view(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+     form = UserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
 
 def profile_view(request):
-    return render(request, 'profile.html')
+    return render(request, 'blog/profile.html')
+    
 
 def home_view(request):
-    return render(request, 'home.html')
+    return render(request, 'blog/home.html')
