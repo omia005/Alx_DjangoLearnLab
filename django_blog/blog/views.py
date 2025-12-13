870223d9b3c5a781cc1211056ff9121e84e0e6e8
Evans Omia <evansomia005@gmail.com>
2025-12-13 23:24:37 +0400

    created the user login view

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 def login_view(request):
-    return render(request, 'blog/login.html')
+    if request.method == 'POST':
+       form = AuthenticationForm(data = request.POST)
+       if form.is_valid():
+           login(request, form.get_user())
+           return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
 
 def logout_view(request):
     return render(request, 'blog/logout.html')
