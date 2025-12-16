e9198769ff0b60601bbdd656b93446331956025c
Evans Omia <evansomia005@gmail.com>
2025-12-16 13:17:10 +0400

    configured urls for the views

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,10 +11,10 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('profile/', views.profile_view, name='profile'),
     path('', views.home_view, name='home'),
-    path('new-post/', PostCreateView.as_view(), name='new-post'),
+    path('post/new/', PostCreateView.as_view(), name='new-post'),
     path('post-list/', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
   
