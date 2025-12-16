85a32f25222e9f3532818e097fe1cf1ddb4f4d69
Evans Omia <evansomia005@gmail.com>
2025-12-16 13:22:16 +0400

    updated the urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='new-post'),
     path('post-list/', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
   
