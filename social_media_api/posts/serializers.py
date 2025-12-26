57dddc93b33dee7e27994f013e6c1e504c32eae7
Evans Omia <evansomia005@gmail.com>
2025-12-26 17:19:01 +0400

    created the posts app

---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import Post, Comment
+
+User = get_user_model()
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Post
+        fields = [
+            'id',
+            'author',
+            'title',
+            'content',
+            'created_at',
+            'updated_at',
+        ]
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id',
+            'post',
+            'author',
+            'content',
+            'created_at',
+            'updated_at',
+        ]
