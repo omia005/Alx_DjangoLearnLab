57831f23ae7636b4dd26bc42d284e641fae3f721
Evans Omia <evansomia005@gmail.com>
2025-12-26 16:19:15 +0400

    project setup and initial commit

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from .models import User
+from .serializers import (RegisterSerializer, LoginSerializer, UserProfileSerializer)
+
+# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = Token.objects.get(user=user)
+
+        return Response({
+            "user": serializer.data,
+            "token": token.key
+        })
+    
+
+class LoginView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response({
+            "token": serializer.validated_data['token']
+        })
+    
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
